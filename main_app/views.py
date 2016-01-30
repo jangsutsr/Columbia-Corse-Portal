731ed906ea43ecbed16d3fad76b2557c1fdbc026
Meyer, Walter <walter.meyer@purchase.edu>
2016-01-29 19:35:44 -0500

    Added modals for reset and registration to login page. Changed UNI to @columbia/barnard email to get both in one shot

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,22 @@ from flask import render_template
 Note server behaviors here are for front-end design only at present.
 '''
 @app.route('/')
-def login():
+@app.route('/index')
+def index():
+	return render_template('index.html')
+
+@app.route('/courses')
+def courses():
+	return render_template('courses.html')
+
+@app.route('/profile')
+def profile():
 	return render_template('profile.html')
 
+@app.route('/forgot')
+def forgot():
+	return render_template('forgot.html')
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
