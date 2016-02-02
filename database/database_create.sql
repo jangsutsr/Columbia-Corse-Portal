9fe5b68ec294a85e4e07b91ab1bb45145ac0bd70
jangsutsr <jangsutsr@gmail.com>
2016-02-02 14:18:24 -0500

    Modify schemas

--- database/database_create.sql
+++ database/database_create.sql
@@ -1,6 +1,11 @@
 -- Entities
-CREATE TABLE cu_er (
-    uni VARCHAR(10),
+CREATE TABLE professor(
+	uni VARCHAR(10) NOT NULL,
+	e_mail TEXT,
+	PRIMARY KEY (uni) );
+
+CREATE TABLE user (
+    uni VARCHAR(10) NOT NULL,
     name VARCHAR(100),
     passwd TEXT,
     e_mail TEXT,
@@ -9,22 +14,29 @@ CREATE TABLE cu_er (
     PRIMARY KEY (uni) );
 
 CREATE TABLE inspector (
-	uni VARCHAR(10),
+	-- Foreigns
+	uni VARCHAR(10) NOT NULL,
+	-- Locals
 	countdown INTEGER,
 	max_course_number INTEGER,
 	PRIMARY KEY (uni),
-	FOREIGN KEY (uni) REFERENCES cu_er );
+	FOREIGN KEY (uni) REFERENCES user );
 
 CREATE TABLE client (
-	uni VARCHAR(10),
+	-- Foreigns
+	uni VARCHAR(10) NOT NULL,
+	-- Locals
 	max_download_rate INTEGER,
 	max_upload_rate INTEGER,
 	PRIMARY KEY (uni),
-	FOREIGN KEY (uni) REFERENCES cu_er );
+	FOREIGN KEY (uni) REFERENCES user );
 
-CREATE TABLE course_permit_instruct (
-	professor_id VARCHAR(10),
-	id VARCHAR(15),
+-- Weak Entities
+CREATE TABLE course_instruct (
+	-- Foreigns
+	prof_uni VARCHAR(10) NOT NULL,
+	-- Locals
+	id VARCHAR(15) NOT NULL,
 	star REAL,
 	doc_permission BOOLEAN,
 	workload REAL,
@@ -32,16 +44,16 @@ CREATE TABLE course_permit_instruct (
 	update_date TIMESTAMP,
 	student_list TEXT[],
 	max_inspector_number INTEGER,
-	PRIMARY KEY (professor_id, id),
-	FOREIGN KEY (professor_id) REFERENCES professor
-		ON DELETE CASCADE);
+	PRIMARY KEY (prof_uni, id),
+	FOREIGN KEY (prof_uni) REFERENCES professor ON DELETE CASCADE);
 
-CREATE TABLE document_delete_upload (
-	contributer VARCHAR(10),
-	id INTEGER,
-	prof VARCHAR(10),
-	course_prof_id VARCHAR(10),
-	course_id VARCHAR(15),
+CREATE TABLE document_upload (
+	-- Foreigns
+	contributer VARCHAR(10) NOT NULL,
+	course_prof VARCHAR(10) NOT NULL,
+	course_id VARCHAR(15) NOT NULL,
+	-- Locals
+	id INTEGER NOT NULL,
 	name TEXT,
 	tags TEXT[],
 	inspectors TEXT[],
@@ -49,17 +61,17 @@ CREATE TABLE document_delete_upload (
 	create_date TIMESTAMP,
 	publish_date TIMESTAMP,
 	raw_file TEXT,
-	PRIMARY KEY (contributer, id),
-	FOREIGN KEY (contributer) REFERENCES client
-		ON DELETE CASCADE,
-	FOREIGN KEY (course_prof_id, course_id) REFERENCES course_permit_instruct,
-	FOREIGN KEY (prof) REFERENCES professor);
+	PRIMARY KEY (contributer, id, course_prof, course_id),
+	FOREIGN KEY (contributer) REFERENCES client ON DELETE CASCADE,
+	FOREIGN KEY (course_prof, course_id) REFERENCES course_instruct);
 
 CREATE TABLE review_write (
-	writer VARCHAR(10),
-	id INTEGER,
-	course_prof_id VARCHAR(10),
-	course_id VARCHAR(15),
+	-- Foreigns
+	writer VARCHAR(10) NOT NULL,
+	course_prof VARCHAR(10) NOT NULL,
+	course_id VARCHAR(15) NOT NULL,
+	-- Locals
+	id INTEGER NOT NULL,
 	inspectors TEXT[],
 	votes INTEGER,
 	create_date TIMESTAMP,
@@ -67,27 +79,39 @@ CREATE TABLE review_write (
 	update_date TIMESTAMP,
 	content TEXT,
 	workload REAL,
-	PRIMARY KEY (writer, id),
-	FOREIGN KEY (writer) REFERENCES client
-		ON DELETE CASCADE
-	FOREIGN KEY (course_prof_id, course_id) REFERENCES course_permit_instruct);
-
-CREATE TABLE professor(
-	uni VARCHAR(10),
-	e_mail TEXT,
-	PRIMARY KEY (uni) );
+	PRIMARY KEY (writer, id, course_prof, course_id),
+	FOREIGN KEY (writer) REFERENCES client ON DELETE CASCADE,
+	FOREIGN KEY (course_prof, course_id) REFERENCES course_instruct);
 
 -- Relations
-CREATE TABLE responsible_for(
+CREATE TABLE permit_course (
+	course_prof VARCHAR(10) NOT NULL,
+	course_id VARCHAR(15) NOT NULL,
+	prof_uni VARCHAR(10) NOT NULL,
+	UNIQUE (course_prof, course_id),
+	PRIMARY KEY (course_prof, course_id, prof_uni),
+	FOREIGN KEY (course_prof, course_id) REFERENCES course_instruct,
+	FOREIGN KEY (prof_uni) REFERENCES professor);
+
+CREATE TABLE delete_doc (
+	doc_contributer VARCHAR(10) NOT NULL,
+	doc_id INTEGER NOT NULL,
+	prof_uni VARCHAR(10) NOT NULL,
+	UNIQUE (doc_contributer, doc_id),
+	PRIMARY KEY (doc_contributer, doc_id, prof_uni),
+	FOREIGN KEY (doc_contributer, doc_id) REFERENCES document_upload,
+	FOREIGN KEY (prof_uni) REFERENCES professor);
+
+CREATE TABLE cover_course(
 	course_prof_id VARCHAR(10) NOT NULL,
 	course_id VARCHAR(15) NOT NULL,
 	course_inspector_uni VARCHAR(10) NOT NULL,
 	PRIMARY KEY (course_prof_id, course_id, course_inspector_uni),
-	FOREIGN KEY (course_prof_id, course_id) REFERENCES course_permit_instruct,
+	FOREIGN KEY (course_prof_id, course_id) REFERENCES course_instruct,
 	FOREIGN KEY (course_inspector_uni) REFERENCES inspector);
 
 CREATE TABLE inspect_doc(
-	inspector_uni VARCHAR(10),
+	inspector_uni VARCHAR(10) NOT NULL,
 	doc_contributer VARCHAR(10) NOT NULL,
 	doc_id INTEGER NOT NULL,
 	PRIMARY KEY (inspector_uni, doc_contributer, doc_id),
@@ -95,43 +119,43 @@ CREATE TABLE inspect_doc(
 	FOREIGN KEY (doc_contributer, doc_id) REFERENCES document_delete_upload);
 
 CREATE TABLE inspect_review(
-	inspector_uni VARCHAR(10),
+	inspector_uni VARCHAR(10) NOT NULL,
 	review_writer VARCHAR(10) NOT NULL,
 	review_id INTEGER NOT NULL,
 	PRIMARY KEY (inspector_uni, review_writer, review_id),
 	FOREIGN KEY (inspector_uni) REFERENCES inspector, 
 	FOREIGN KEY (review_writer, review_id) REFERENCES review_write);
 
-CREATE TABLE report_on(
-	client_uni VARCHAR(10),
-	doc_contributer VARCHAR(10),
-	doc_id INTEGER,
+CREATE TABLE report_doc(
+	client_uni VARCHAR(10) NOT NULL,
+	doc_contributer VARCHAR(10) NOT NULL,
+	doc_id INTEGER NOT NULL,
 	PRIMARY KEY (client_uni, doc_contributer, doc_id),
 	FOREIGN KEY (client_uni) REFERENCES client,
-	FOREIGN KEY (doc_contributer, doc_id) REFERENCES document_delete_upload);
+	FOREIGN KEY (doc_contributer, doc_id) REFERENCES document_upload);
 
 CREATE TABLE vote_doc(
-	client_uni VARCHAR(10),
-	doc_contributer VARCHAR(10),
-	doc_id INTEGER,
+	client_uni VARCHAR(10) NOT NULL,
+	doc_contributer VARCHAR(10) NOT NULL,
+	doc_id INTEGER NOT NULL,
 	PRIMARY KEY (client_uni, doc_contributer, doc_id),
 	FOREIGN KEY (client_uni) REFERENCES client,
-	FOREIGN KEY (doc_contributer, doc_id) REFERENCES document_delete_upload);
+	FOREIGN KEY (doc_contributer, doc_id) REFERENCES document_upload);
 
-CREATE TABLE download( 
-	client_uni VARCHAR(10),
-	doc_contributer VARCHAR(10),
-	doc_id INTEGER,
+CREATE TABLE download_doc( 
+	client_uni VARCHAR(10) NOT NULL,
+	doc_contributer VARCHAR(10) NOT NULL,
+	doc_id INTEGER NOT NULL,
 	PRIMARY KEY (client_uni, doc_contributer, doc_id),
 	FOREIGN KEY (client_uni) REFERENCES client,
-	FOREIGN KEY (doc_contributer, doc_id) REFERENCES document_delete_upload);
+	FOREIGN KEY (doc_contributer, doc_id) REFERENCES document_upload);
 
 CREATE TABLE vote_review(
-	client_uni VARCHAR(10),
-	review_writer VARCHAR(10),
-	review_id INTEGER,
+	client_uni VARCHAR(10) NOT NULL,
+	review_writer VARCHAR(10) NOT NULL,
+	review_id INTEGER NOT NULL,
 	PRIMARY KEY (client_uni, review_writer, review_id),
 	FOREIGN KEY (client_uni) REFERENCES client,
-	FOREIGN KEY (review_writer, review_id) REFERENCES client);
+	FOREIGN KEY (review_writer, review_id) REFERENCES review_write);
 
 
