410c624c28cf000d94c6be9d13f49f727a3aa1bd
Ubuntu <Aaron@TRY-DB.m4xgqhcpip2elkqgvxcxvicr0a.bx.internal.cloudapp.net>
2016-02-15 16:27:42 +0000

    Modify schema

--- database_aaron/create_database.sql
+++ database_aaron/create_database.sql
@@ -33,6 +33,15 @@ CREATE TABLE course (
     PRIMARY KEY (prof, cid),
     FOREIGN KEY (prof) REFERENCES professor (id)
 );
+CREATE TABLE take (
+    usr VARCHAR(10) NOT NULL,
+    course INTEGER NOT NULL,
+    course_prof INTEGER NOT NULL,
+    PRIMARY KEY (usr, course, course_prof),
+    FOREIGN KEY (usr) REFERENCES usr (uni),
+    FOREIGN KEY (course, course_prof)
+        REFERENCES course (cid, prof)
+);
 CREATE TABLE relate (
     course_prof INTEGER NOT NULL,
     course_id INTEGER NOT NULL,
@@ -45,24 +54,30 @@ CREATE TABLE relate (
 CREATE TABLE document (
     uni VARCHAR(10) NOT NULL,
     did INTEGER NOT NULL,
+    cid INTEGER NOT NULL,
+    prof INTEGER NOT NULL,
     name TEXT,
     file_location TEXT,
     create_date DATE,
     star REAL,
     comment_count INTEGER,
     report_count INTEGER,
-    PRIMARY KEY (uni, did),
-    FOREIGN KEY (uni) REFERENCES usr (uni)
+    PRIMARY KEY (did),
+    FOREIGN KEY (uni) REFERENCES usr (uni),
+    FOREIGN KEY (cid, prof) REFERENCES course (cid, prof)
 );
 CREATE TABLE review (
     uni VARCHAR(10) NOT NULL,
     rid INTEGER NOT NULL,
+    cid INTEGER NOT NULL,
+    prof INTEGER NOT NULL,
     content TEXT,
     create_date DATE,
     update_date DATE,
     star REAL,
     comment_count INTEGER,
     report_count INTEGER,
-    PRIMARY KEY (uni, rid),
-    FOREIGN KEY (uni) REFERENCES usr (uni)
+    PRIMARY KEY (rid),
+    FOREIGN KEY (uni) REFERENCES usr (uni),
+    FOREIGN KEY (cid, prof) REFERENCES course (cid, prof)
 );
