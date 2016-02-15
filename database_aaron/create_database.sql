f18678464a12cb3b298d9359049f934e30900a36
Meyer, Walter <walter.meyer@purchase.edu>
2016-02-15 16:12:42 -0500

    Adding SQL schema modifications as discussed

--- database_aaron/create_database.sql
+++ database_aaron/create_database.sql
@@ -1,10 +1,8 @@
 CREATE TABLE usr (
-    uni VARCHAR(10) NOT NULL,
+    name TEXT NOT NULL,
     passwd TEXT NOT NULL,
-    e_mail TEXT,
-    max_download_rate INTEGER,
-    download_count INTEGER,
-    PRIMARY KEY (uni)
+    e_mail TEXT NOT NULL,
+    PRIMARY KEY (e_mail)
 );
 CREATE TABLE department (
     id INTEGER NOT NULL,
@@ -23,24 +21,24 @@ CREATE TABLE affiliate (
     FOREIGN KEY (dept) REFERENCES department (id),
     FOREIGN KEY (prof) REFERENCES professor (id)
 );
+-- Weak Entity [course] ==> <teach>
 CREATE TABLE course (
     prof INTEGER NOT NULL,
     cid INTEGER NOT NULL,
     name TEXT,
     workload REAL,
     star REAL,
-    comment_count INTEGER,
+    vote_count INTEGER,
     PRIMARY KEY (prof, cid),
     FOREIGN KEY (prof) REFERENCES professor (id)
 );
 CREATE TABLE take (
-    usr VARCHAR(10) NOT NULL,
+    usr TEXT NOT NULL,
     course INTEGER NOT NULL,
     course_prof INTEGER NOT NULL,
     PRIMARY KEY (usr, course, course_prof),
-    FOREIGN KEY (usr) REFERENCES usr (uni),
-    FOREIGN KEY (course, course_prof)
-        REFERENCES course (cid, prof)
+    FOREIGN KEY (usr) REFERENCES usr (e_mail),
+    FOREIGN KEY (course, course_prof) REFERENCES course (cid, prof)
 );
 CREATE TABLE relate (
     course_prof INTEGER NOT NULL,
@@ -48,36 +46,37 @@ CREATE TABLE relate (
     dept INTEGER NOT NULL,
     PRIMARY KEY (course_prof, course_id, dept),
     FOREIGN KEY (dept) REFERENCES department (id),
-    FOREIGN KEY (course_prof, course_id)
-        REFERENCES course (prof, cid)
+    FOREIGN KEY (course_prof, course_id) REFERENCES course (prof, cid)
 );
+-- Weak Entity [document] ==> <upload_to>
 CREATE TABLE document (
-    uni VARCHAR(10) NOT NULL,
-    did INTEGER NOT NULL,
+    did SERIAL,
+    usr TEXT NOT NULL,
     cid INTEGER NOT NULL,
     prof INTEGER NOT NULL,
     name TEXT,
     file_location TEXT,
     create_date DATE,
     star REAL,
-    comment_count INTEGER,
+    vote_count INTEGER,
     report_count INTEGER,
     PRIMARY KEY (did),
-    FOREIGN KEY (uni) REFERENCES usr (uni),
+    FOREIGN KEY (usr) REFERENCES usr (e_mail),
     FOREIGN KEY (cid, prof) REFERENCES course (cid, prof)
 );
+-- Weak Entity [document] ==> <write_for>
 CREATE TABLE review (
-    uni VARCHAR(10) NOT NULL,
-    rid INTEGER NOT NULL,
-    cid INTEGER NOT NULL,
-    prof INTEGER NOT NULL,
+    rid SERIAL,
+    usr TEXT NOT NULL UNIQUE,
+    cid INTEGER NOT NULL UNIQUE,
+    prof INTEGER NOT NULL UNIQUE,
     content TEXT,
     create_date DATE,
     update_date DATE,
     star REAL,
-    comment_count INTEGER,
+    vote_count INTEGER,
     report_count INTEGER,
     PRIMARY KEY (rid),
-    FOREIGN KEY (uni) REFERENCES usr (uni),
+    FOREIGN KEY (usr) REFERENCES usr (e_mail),
     FOREIGN KEY (cid, prof) REFERENCES course (cid, prof)
 );
