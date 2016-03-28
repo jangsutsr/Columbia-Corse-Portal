f6c9a8b5d84240d2bd12da16c41c459246cb77bb
Meyer, Walter <walter.meyer@purchase.edu>
2016-03-28 12:07:24 -0400

    Added functionality for rating reviews

---
+++ static/scripts/review.js
@@ -0,0 +1,9 @@
+// JS to update form action for rating a particular review
+
+$('#rateareview').on('click', function (event) {
+  var rid = $('#rateareview').data('rid') // Get the rid that was clicked
+  var act = document.getElementById('rate-review').action
+  act = act.concat('/'+rid)
+  // update the action with the correction URI
+  document.getElementById('rate-review').setAttribute("action", act)
+})
