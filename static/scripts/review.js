1ca25d8ae61ece377ad456e283249ce983c202c7
Meyer, Walter <walter.meyer@purchase.edu>
2016-03-28 20:31:18 -0400

    fix report

--- static/scripts/review.js
+++ static/scripts/review.js
@@ -7,3 +7,11 @@ $('#rateareview').on('click', function (event) {
   // update the action with the correction URI
   document.getElementById('rate-review').setAttribute("action", act)
 })
+
+$('#rateadocument').on('click', function (event) {
+  var rid = $('#rateadocument').data('rid') // Get the rid that was clicked
+  var act = document.getElementById('rate-document').action
+  act = act.concat('/'+rid)
+  // update the action with the correction URI
+  document.getElementById('rate-document').setAttribute("action", act)
+})
