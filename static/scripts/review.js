6952da20afef475182201416343a312b818f79e9
Meyer, Walter <walter.meyer@purchase.edu>
2016-03-28 20:47:37 -0400

    added js

--- static/scripts/review.js
+++ static/scripts/review.js
@@ -8,10 +8,11 @@ $('#rateareview').on('click', function (event) {
   document.getElementById('rate-review').setAttribute("action", act)
 })
 
+// JS to update form action for rating a particular review
 $('#rateadocument').on('click', function (event) {
-  var rid = $('#rateadocument').data('rid') // Get the rid that was clicked
+  var did = $('#rateadocument').data('did') // Get the rid that was clicked
   var act = document.getElementById('rate-document').action
-  act = act.concat('/'+rid)
+  act = act.concat('/'+did)
   // update the action with the correction URI
   document.getElementById('rate-document').setAttribute("action", act)
 })
